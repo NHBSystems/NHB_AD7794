acce5824ab89f56d93df02b5aaffaeda97e29be4
Jaimy Juliano <jjuliano77@gmail.com>
2020-09-08 18:02:59 -0500

    Changed to MIT License

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NHB_AD7794
 Arduino Library for the Analog Devices AD7794 24bit ADC
 
-This essentially just a new fork of the [JJ_AD7794](https://github.com/jjuliano77/JJ_AD7794) library.
+This is essentially just a new fork of the [JJ_AD7794](https://github.com/jjuliano77/JJ_AD7794) library. I have also changed to an MIT license.
 
 This library has been tested with ATMEGA328, ATMEGA32u4, SAMD21, Teensy 3.2, and Esp8266* (The Esp8266 has goofy SPI and requires a some workarounds).
--- examples/Read_Thermocouple/Read_Thermocouple.ino
+++ examples/Read_Thermocouple/Read_Thermocouple.ino
@@ -9,22 +9,29 @@
   1 between the GND terminal and the TC- terminal and 1 between the EXV(+)
   terminal and the TC+ terminal. (See diagram that should be with this file)
 
-  Copyright (C) 2019  Jaimy Juliano
-
   This file is part of the JJ_AD7794 library.
 
-  This library is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 2 of the License, or
-  (at your option) any later version.
+  MIT License
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Copyright (C) 2010,2019  Jaimy Juliano, NHBSystems
 
-  You should have received a copy of the GNU General Public License
-  along with this library. If not, see <http://www.gnu.org/licenses/>.
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #include <SPI.h>
